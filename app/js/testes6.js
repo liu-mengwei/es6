e05c934fb7e3b02350f71eb8fb77035e483a269f
liu-mengwei <542221757@qq.com>
2017-06-18 10:30:08 +0800

    string / number /  regExp

--- app/js/testes6.js
+++ app/js/testes6.js
@@ -26,11 +26,25 @@ function print() {
 }
 
 
+//解构赋值，左右形式相同
 {
     let {a:[{title:vtitle}]} = {a: [{title: 'title'}]};
     print(vtitle);
 
     var reg1 = /user\d/g;
-    print(reg1.exec('user1user2').index, reg1.exec('user1user2').index,reg1.exec('user1user2')
-    ,reg1.exec('user1').index);
+    print(reg1.exec('user1user2').index, reg1.exec('user1user2').index, reg1.exec('user1user2')
+        , reg1.exec('user1').index);
+}
+
+{
+    let student = {name: 'mengmeng', age: '16'};
+
+    //标签模板
+    print(change` i am student ,name is ${student.name},age is ${student.age}`);
+    function change(s1, v1, v2) {
+        //print(s1);
+        //print(v1);
+        //print(v2);
+        return v1 + s1 + v2;
+    }
 }
--- server/public/js/index.js
+++ server/public/js/index.js
@@ -63,6 +63,10 @@
 
 	var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
+	var _templateObject = _taggedTemplateLiteral([' i am student ,name is ', ',age is ', ''], [' i am student ,name is ', ',age is ', '']);
+
+	function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
 	/**
 	 * Created by liumw on 2017/6/14.
 	 */
@@ -95,6 +99,7 @@
 	    print(va, vb);
 	}
 
+	//解构赋值，左右形式相同
 	{
 	    var _a = { a: [{ title: 'title' }] },
 	        _a$a = _slicedToArray(_a.a, 1),
@@ -104,6 +109,20 @@
 
 	    var reg1 = /user\d/g;
 	    print(reg1.exec('user1user2').index, reg1.exec('user1user2').index, reg1.exec('user1user2'), reg1.exec('user1').index);
+	}
+
+	{
+	    var change = function change(s1, v1, v2) {
+	        //print(s1);
+	        //print(v1);
+	        //print(v2);
+	        return v1 + s1 + v2;
+	    };
+
+	    var student = { name: 'mengmeng', age: '16' };
+
+	    //标签模板
+	    print(change(_templateObject, student.name, student.age));
 	}
 
 /***/ })
